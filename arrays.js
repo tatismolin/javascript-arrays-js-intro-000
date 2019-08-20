7501c6abd3b0170298d12582d01d643727e4bc79
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 22:45:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
+  array.unshift();
   return array;
 }
 
