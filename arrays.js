988481af898d1bbd2b3ba79a2f5d683e8fcd6c1f
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 22:41:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,8 @@ function addElementToBeginningOfArray(array, element) {
 
 
 function destructivelyAddElementToBeginningOfArray(array, element){ //need fix
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element){
