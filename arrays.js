09c01488685a7b05b519c1e1bb45836aa8c54102
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 22:42:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){ //need fix
-  var array = [...array, element];
+  array.push(element);
   return array;
 }
 
